255e83741495fba8eb3adf4306fc8ab276a29a28
Ilim1111 <urmatberdieva5@gmail.com>
2023-10-16 17:23:10 +0300

    fixed size of massive

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     }
 
     public static void math() {
-        double[] numbers = {8.0, -2.0, -4.0, 2.0, 3.0, 6.0, -7.0};
+        double[] numbers = {8.0, -2.0, -4.0, 2.0, 3.0, 6.0, -7.0, 2.0, 3.0, 2.0, 6.0, 1.0, 7.0, 8.0, 5.0};
         double sum = 0;
         int count = 0;
         boolean foundNegative = false;
